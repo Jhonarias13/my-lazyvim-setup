4aee61dd66f143356d73e5ac36f16150c83dbe2d
Jhon Freiman Arias <jhonarias1322@gmail.com>
2023-10-27 07:24:28 -0500

    init lazyvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "haishanh/night-owl.vim", name = "night-owl" },
+  { "safv12/andromeda.vim" },
+  { "tjdevries/colorbuddy.nvim", branch = "dev" },
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
