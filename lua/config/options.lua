ed66fc2efc054bdd2d63a21f360c80813afeaa4b
Jhon Freiman Arias <jhonarias1322@gmail.com>
2023-10-29 07:20:16 -0500

    anu=y changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- This keybinding uses kj as the scape key to exit a mode
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = true })
+
+-- this command fold all code with Leader + k + 0 (zero) using foldmethod = indent
+vim.keymap.set("n", "<Leader>k0", "<cmd>set foldmethod=indent<CR> zM")
+
+-- this command unfold all code with Leader + k + j
+vim.keymap.set("n", "<Leader>kj", "zR")
+
+-- This keymap set up space + w to save and space + q to quit
+-- vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
 --
--- add neotree show right position
+-- Configuración específica para nvim-notify
+vim.g.notify_position = "topleft"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,31 @@
 return {
   { "haishanh/night-owl.vim", name = "night-owl" },
-  { "safv12/andromeda.vim" },
+
+  -- andromeda
+  {
+    "nobbmaestro/nvim-andromeda",
+    requires = { "tjdevries/colorbuddy.nvim", branch = "dev" },
+  },
+
   { "tjdevries/colorbuddy.nvim", branch = "dev" },
+
   { "folke/tokyonight.nvim" },
+
   { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "tokyonight-night",
     },
   },
 }
